70be537acdb0451f8cc3b425a3e66faf099bd68a
Darsh Wala <darshwala@gmail.com>
2026-05-29 18:36:56 +0530

    used state to store memes array from the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
+import Header from './components/header'
+import Main from './components/main'
 
 function App() {
   return(
-    <p></p>
+    <>
+      <Header/>
+      <Main/>
+    </>
   )
 }
 
---
+++ src/components/main.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function Main() {
+  const [meme, setMeme] = React.useState({
+    topText: "One does not",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = React.useState({});
+  
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  // console.log(allMemes[0])
+
+  // function change(){
+  //     setMeme(prevVal => ({
+  //         ...prevVal,
+  //         imageUrl : allMemes[0]
+  //     }))
+  // }
+  // React.useEffect (() => {
+  //     fetch("https://api.imgflip.com/get_memes")
+  //         .then(res => res.json())
+  //         .then(data => setMeme(prevVals => ({
+  //             ...prevVals,
+  //             imageUrl : data.data.memes[99].url
+  //         })))
+  // } , [])
+
+  function handleChange(e) {
+    const { value, name } = e.currentTarget;
+
+    setMeme((prevVals) => ({
+      ...prevVals,
+      [name]: value,
+    }));
+
+    console.log(value);
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
+          X
+        </label>
+        <button>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
+}
